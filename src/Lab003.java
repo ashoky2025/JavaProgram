8210b949be425ce1449ecc7400b34671d0f2c47b
Ashok Yadav <ashoky2005@gmail.com>
2025-01-25 12:33:16 +0530

    Push And Commit

---
+++ src/Lab003.java
@@ -0,0 +1,11 @@
+public class Lab003 {
+
+
+    public static void main(String[] args) {
+        System.out.println("Lab002");
+    }
+
+
+
+
+}
---
+++ src/ex_01_Java_Basics/Lab002.java
@@ -0,0 +1,11 @@
+package ex_01_Java_Basics;
+
+public class Lab002 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+
+
+
+}
